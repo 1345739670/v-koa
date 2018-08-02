f539d3b0d049d3eed5c07a50c25c0a206c1c369e
Mr.stupid bird <1345739670@qq.com>
2018-08-02 20:58:42 +0800

    Implement a simple API server using koa-router

--- server/index.js
+++ server/index.js
@@ -1,18 +1,26 @@
 const Koa = require('koa');
-const mongoose = require('mongoose');
 const { connect,initSchemas } = require('./database/init')
+const router = require('./router')
+const port = 9876;
 ;(async () => {
     await connect()
 
     initSchemas()
 
     // require('./tasks/movie')
-    require('./tasks/api')
+    // require('./tasks/api')
     
 })()
 
 const app = new Koa();
+
+app
+    .use(router.routes())
+    .use(router.allowedMethods())
+
 app.use(async (ctx, next)=>{
     ctx.body = '电影首页'
 })
-app.listen(6357);
+app.listen(port);
+
+// console.log(`Server running at http://127.0.0.1:${port}/`);
